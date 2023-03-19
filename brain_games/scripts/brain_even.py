46f9206f6d14f864373ce3fcae4a4295303a2180
starshina6 <125303949+starshina6@users.noreply.github.com>
2023-03-19 23:47:57 +0300

    five step

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.even  import chek_even
+
+
+def main():
+    print('Welcome to the Brain Games!')    
+    chek_even(3)
+
+
+if __name__ == '__main__':
+    main()
