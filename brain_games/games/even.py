5a07132e38a9def49c4a1a25257bde4637e26bff
starshina6 <125303949+starshina6@users.noreply.github.com>
2023-05-09 19:23:51 +0300

    correct five

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,13 +7,16 @@ MAX_VALUE = 30
 
 def is_even(number):
     if number % 2 == 0:
-        return 'yes'
+        return True
     else:
-        return 'no'
+        return False
 
 
 def get_round():
     question = random.randint(MIN_VALUE, MAX_VALUE)
-    correct_answer = is_even(question)
+    if is_even(question):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
 
     return question, correct_answer
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,14 +1,21 @@
 import random
 
 RULE = 'What number is missing in the progression?'
+START_MIN_VALUE = 0
+START_MAX_VALUE = 10
+LEN_MIN_VALUE = 5
+LEN_MAX_VALUE = 10
+STEP_MIN_VALUE = 2
+STEP_MAX_VALUE = 10
 
 
 def get_progression():
-    first_number = random.randint(0, 10)
+    first_number = random.randint(START_MIN_VALUE, START_MAX_VALUE)
     progression = [first_number]
 
-    progression_len = random.randint(5, 10)
-    step = random.randint(2, 10)
+    progression_len = random.randint(LEN_MIN_VALUE, LEN_MAX_VALUE)
+    step = random.randint(STEP_MIN_VALUE, STEP_MAX_VALUE)
+
     i = 1
 
     while i < progression_len:
